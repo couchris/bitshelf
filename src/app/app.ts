aba6a1a9749e262af3992853927669fb867457c5
Christopher Couto <couchris@sheridancollege.ca>
2026-04-05 11:06:11 -0400

    fix: reload GameList after form submit using ViewChild

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameSearch } from './game-search/game-search';
 import { GameList } from './game-list/game-list';
@@ -15,6 +15,8 @@ import { SortStrategy } from './sort-strategy';
   styleUrls: ['./app.scss']
 })
 export class AppComponent {
+  @ViewChild(GameList) gameList!: GameList;
+
   showForm = false;
   gameToEdit: Game | null = null;
   activeSortStrategy: SortStrategy | null = null;
@@ -28,6 +30,12 @@ export class AppComponent {
   onEditGame(game: Game): void {
     this.gameToEdit = game;
     this.showForm = true;
+  }
+
+  onFormSubmitted(): void {
+    this.showForm = false;
+    this.gameToEdit = null;
+    this.gameList.loadGames();
   }
 
   onFormClosed(): void {
